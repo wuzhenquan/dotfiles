29112e3ec21e0b566167e5645656b1428c2f609a
perastra.cc <zhenquan.me@gmail.com>
2026-05-16 21:08:00 +0800

    feat(fish): add cross-platform Homebrew initialization

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,7 +5,13 @@ if status is-interactive
 end
 
 # Homebrew's environment variables
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+# Check if we are on Apple Silicon macOS
+if test -d /opt/homebrew
+    /opt/homebrew/bin/brew shellenv | source
+# Check if we are on Linux
+else if test -d /home/linuxbrew/.linuxbrew
+    /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
+end
 
 # Set up fzf key bindings
 fzf --fish | source
