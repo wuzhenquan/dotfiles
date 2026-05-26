4167ee9e14208ff27b248e8a29b363d157ec686d
Tristan <zhenquan.me@gmail.com>
2026-05-26 20:41:54 +0800

    fix(fish): use set_color normal in prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt --description 'Write out the prompt'
     set -l last_status $status
-    set -l normal (set_color --reset)
+    set -l normal (set_color normal)
     set -l status_color (set_color brgreen)
     set -l cwd_color (set_color $fish_color_cwd)
     set -l vcs_color (set_color brpurple)
