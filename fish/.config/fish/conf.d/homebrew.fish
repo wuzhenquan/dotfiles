16d38f758f5801769e1e87cee334aa6133728a60
Tristan <zhenquan.me@gmail.com>
2026-05-27 18:55:33 +0800

    refector(fish): move Homebrew initialization to conf.d/homebrew.fish

---
+++ fish/.config/fish/conf.d/homebrew.fish
@@ -0,0 +1,7 @@
+# Check if we are on Apple Silicon macOS
+if test -d /opt/homebrew
+    /opt/homebrew/bin/brew shellenv | source
+# Check if we are on Linux
+else if test -d /home/linuxbrew/.linuxbrew
+    /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
+end
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,3 @@
-# Homebrew's environment variables
-# Check if we are on Apple Silicon macOS
-if test -d /opt/homebrew
-    /opt/homebrew/bin/brew shellenv | source
-# Check if we are on Linux
-else if test -d /home/linuxbrew/.linuxbrew
-    /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
-end
-
 # Set up fzf key bindings
 fzf --fish | source
 
