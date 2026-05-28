19af83e75da4f2f5ae5e262293b9a1aff8f916a1
Tristan <zhenquan.me@gmail.com>
2026-05-28 16:28:32 +0800

    feat(fish): add common abbreviations for git, and editor commands
    Add fish abbreviations for common git commands (gs, gp, gc, gco, gcm,
    gpo) and editor shortcuts (v for nvim, lg for lazygit).

---
+++ fish/.config/fish/conf.d/abbreviations.fish
@@ -0,0 +1,9 @@
+abbr --add gs "git status"
+abbr --add gp "git push"
+abbr --add gc "git commit"
+abbr --add gco "git checkout"
+abbr --add gcm 'git commit -m'
+abbr --add gpo 'git push origin'
+
+abbr --add v nvim
+abbr --add lg lazygit
